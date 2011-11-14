8de438d04a0139e7aae37bcd12e4a48777c4f8fb
John <john.mcdonald100@email.wsu.edu>
2011-11-14 12:42:33 -0800

    Added cheshire to my test file.

--- Makefile
+++ Makefile
@@ -11,11 +11,11 @@ cma.o: cma.c cma.h
 ma.exe: ma.o cma.o
 	gcc ma.o cma.o -o ma.exe
 
-john_mcdonald_test.o: john_mcdonald_test.c
+john_mcdonald_test.o: john_mcdonald_test.c cma.h
 	gcc -c john_mcdonald_test.c
 
-john_mcdonald_test.exe: john_mcdonald_test.o
-	gcc -o john_mcdonald_test.exe john_mcdonald_test.o
+john_mcdonald_test.exe: john_mcdonald_test.o cma.o
+	gcc -o john_mcdonald_test.exe john_mcdonald_test.o cma.o
 
 clean:
 	-rm $(EXES)
--- john_mcdonald_test.c
+++ john_mcdonald_test.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+#include "cma.h"
+
+#define MSIZE 1024*16
+unsigned char mem[MSIZE];
+
+char cat[]="\n\
+	           .'\\   /`.\n\
+                 .'.-.`-'.-.`.\n\
+            ..._:   .-. .-.   :_...\n\
+          .'    '-.(o ) (o ).-'    `.\n\
+         :  _    _ _`~(_)~`_ _    _  :\n\
+        :  /:   ' .-=_   _=-. `   ;\\  :\n\
+        :   :|-.._  '     `  _..-|:   :\n\
+         :   `:| |`:-:-.-:-:'| |:'   :\n\
+          `.   `.| | | | | | |.'   .'\n\
+            `.   `-:_| | |_:-'   .'\n\
+              `-._   ````    _.-'\n\
+                  ``-------''  \n";
 
 int main()
 {
-	printf("test\n");
+	char* string;
+	class_memory(mem,MSIZE);
+
+	string = (char*)class_malloc(sizeof(cat));
+	strcpy(string,cat);
+
+	printf("%s\n",string);
+
+	class_free(string);
+	
+	class_stats();
+
 	return 0;
 }
